1c9a26665a0c07aa175a3a822919aaa9575cbf4e
Lydia Greco <lydiagreco@Lydias-MacBook-Air.local>
2018-01-21 13:10:38 -0700

    Relational connection between models and rails

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,11 +3,11 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   describe "creation" do
     before do
-      @user = User.create(email: "test@test.com", password: "asdfasdf", password_confirmation: "asdfasdf")
+      @user = User.create(email: "test@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "Lydia", last_name: "Speirs")
     end
-  
+
     it "can be created" do
-      expect(user).to be_valid
+      expect(@user).to be_valid
     end
 
     it "cannot be created without first_name, last_name" do
