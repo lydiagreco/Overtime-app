312ddb2e68aa94888374090b15c993a501370bdf
Lydia Greco <lydiagreco@Lydias-MacBook-Air.local>
2018-01-21 18:11:31 -0700

    Added form to view files

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,9 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: [:show]
- def index
-   @posts = Post.all
- end
+  before_action :set_post, only: [:show, :edit, :update]
+  
+  def index
+    @posts = Post.all
+  end
 
   def new
     @post = Post.new
@@ -16,6 +17,17 @@ class PostsController < ApplicationController
       redirect_to @post, notice: 'Your post was created successfully'
     else
       render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @post.update(post_params)
+      redirect_to @post, notice: 'Your post was created successfully'
+    else
+      render :edit
     end
   end
 
