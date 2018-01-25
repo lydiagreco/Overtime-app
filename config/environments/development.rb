8f3135e26c32454a86f8fb0699dc0b2cba472213
Lydia Greco <lydiagreco@Lydias-MacBook-Air.local>
2018-01-25 15:03:38 -0700

    getting ready to push to heroku

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -18,8 +18,9 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
-  config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.perform_caching = false
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   config.active_support.deprecation = :log
   config.active_record.migration_error = :page_load
   config.assets.debug = true
