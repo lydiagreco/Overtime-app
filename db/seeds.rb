d55010725be288dc8031e3841307deea70672f64
Lydia Greco <lydiagreco@Lydias-MacBook-Air.local>
2018-01-24 18:14:36 -0700

    Added admin functionality, updated seeds file, ran migration for admin, fixed factory bot update and server issues

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,8 +16,8 @@ AdminUser.create(email: "admin@test.com",
 
 puts "1 Admin User created"
 
-100.times do |post|
-  Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id, overtime_request: 2.5)
+100.times do |audit_log|
+  AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 6.days))
 end
 
-puts "100 Posts have been created"
+puts "100 audit logs have been created"
