974b8ed08f5afe76699306b9f7e82a908d9abd53
Lydia Greco <lydiagreco@Lydias-MacBook-Air.local>
2018-01-25 13:32:41 -0700

    Implementing employee functionality and instal approval

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,14 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: [:show, :edit, :update, :destroy]
+  before_action :set_post, only: [:show, :edit, :update, :destroy, :approve]
   
   def index
     @posts = Post.posts_by(current_user).page(params[:page]).per(10)
   end
+
+  def approve
+    authorize @post
+    @post.approved!
+    redirect_to root_path, notice: "The post has been approved"
   end
 
   def new
@@ -22,32 +27,31 @@ class PostsController < ApplicationController
   end
 
   def edit
-   authorize @post
+    authorize @post
   end
 
   def update
-   authorize @post
-
-   if @post.update(post_params)
-     redirect_to @post, notice: 'Your post was edited successfully'
-   else
-     render :edit
-   end
+    authorize @post
+    
+    if @post.update(post_params)
+      redirect_to @post, notice: 'Your post was edited successfully'
+    else
+      render :edit
+    end
   end
-
 
   def show
   end
 
   def destroy
-   @post.delete
-   redirect_to posts_path, notice: 'Your post was deleted successfully'
+    @post.delete
+    redirect_to posts_path, notice: 'Your post was deleted successfully'
   end
 
   private
 
     def post_params
-     params.require(:post).permit(:date, :rationale, :status, :overtime_request)
+      params.require(:post).permit(:date, :rationale, :status, :overtime_request)
     end
 
     def set_post
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  resources :audit_logs, except: [:new, :edit, :destory]
+  resources :audit_logs, except: [:new, :edit, :destory] do
+    member do
+      get :confirm
+    end
+  end
   
   namespace :admin do
     resources :users
@@ -9,7 +13,12 @@ Rails.application.routes.draw do
     root to: "users#index"
   end
 
-  resources :posts
+  resources :posts do
+    member do
+      get :approve
+    end
+  end
+
   devise_for :users, skip: [:registrations]
   root to: 'static#homepage'
 end
